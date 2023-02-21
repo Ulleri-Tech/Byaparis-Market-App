459656fa5116705cb55e4dd79ab6a8e60cef4333
Binij Shrestha <shresthabinij@gmail.com>
2023-02-21 17:02:38 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Byaparis-MarketPlace
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Welcome to Byapari's MarketPlace</h1>
+    </div>
+  )
+}
+
+export default App
