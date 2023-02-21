4e1e2053a21071cb84d40af53e3cb9bfd8fca388
Binij Shrestha <shresthabinij@gmail.com>
2023-02-21 18:19:09 +0545

    feat: React Bootstrap

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,8 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
